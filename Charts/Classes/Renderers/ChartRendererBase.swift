e531576e7cc35d104d5e4f2670b9ab5462263868
Mathias Claassen <mathias@xmartlabs.com>
2015-12-03 10:38:54 -0300

    fixed bug where charts would not draw labels when frame was smaller than view

--- Charts/Classes/Renderers/ChartRendererBase.swift
+++ Charts/Classes/Renderers/ChartRendererBase.swift
@@ -57,8 +57,8 @@ public class ChartRendererBase: NSObject
         
         let subLow = (low % xAxisModulus == 0) ? xAxisModulus : 0
         
-        _minX = max((low / xAxisModulus) * (xAxisModulus) - subLow, 0)
         _maxX = min((high / xAxisModulus) * (xAxisModulus) + xAxisModulus, Int(chart.chartXMax))
+        _minX = min(max((low / xAxisModulus) * (xAxisModulus) - subLow, 0), _maxX)
     }
 }
         
